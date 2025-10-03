de21fcb90d4e6037d5fc4bcc57803a1bc388b926
Ivar <63834@gymnasium.ax>
2025-10-03 13:52:11 +0300

    Uppstart av SSP projektet

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,50 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <h1>Sten, sax, påse!</h1>
   </header>
 
   <main>
-    <TheWelcome />
+    <div class="knapprad">
+      <button>Sten</button>
+      <button>Sax</button>
+      <button>Påse</button>
+    </div>
+    <div class="resultat">
+      <p id="resultat">Du vann!</p>
+    </div>
+    <div class="score">
+      <p><span id="spelare">0</span> - <span id="dator">0</span></p>
+    </div>
   </main>
 </template>
 
 <style scoped>
 header {
-  line-height: 1.5;
+  text-align: center;
+  margin-bottom: 1.2em;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+button {
+  padding: 0.6em 1.2em;
+  font-size: 1.2em;
+  background-color: cadetblue;
+  border: 1px solid darkcyan;
+  border-radius: 5px;
+  cursor: pointer;
 }
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.knapprad {
+  display: flex;
+  justify-content: center;
+  gap: 0.6em;
+}
+.resultat {
+  font-size: 1.2em;
+  text-align: center;
+  margin: 1.2em 0;
+}
+.score {
+  font-size: 1.2em;
+  text-align: center;
 }
 </style>

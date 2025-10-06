c2098a6367787153c7f073ef69d90c0fce0e297e
Ivar <63834@gymnasium.ax>
2025-10-06 15:13:49 +0300

    lagg till att man kan vinna,förlora o oavgjort. Nollställa poäng knapp

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,65 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+
+const score = ref({ spelare: 0, dator: 0 })
+const resultat = ref('Du vann!')
+
+function spelarval(e) {
+  let buttons = document.getElementsByClassName('alternativ')
+  for (let b of buttons) {
+    b.classList.remove('spelarval')
+  }
+  e.target.classList.add('spelarval')
+  datorval()
+  hittaVinnare()
+}
+function datorval() {
+  let val = Math.floor(Math.random() * 3)
+  let alternativ = ['Sten', 'Sax', 'Påse']
+  let buttons = document.getElementsByClassName('alternativ')
+  for (let b of buttons) {
+    b.classList.remove('datorval')
+    b.title = ''
+    if (b.innerText === alternativ[val]) {
+      b.classList.add('datorval')
+      b.title = 'Datorns val'
+    }
+  }
+}
+function hittaVinnare() {
+  let buttons = document.getElementsByClassName('alternativ')
+  for (let b of buttons) {
+    if (b.classList.contains('spelarval')) {
+      var spelarval = b.textContent
+    }
+    if (b.classList.contains('datorval')) {
+      var datorval = b.textContent
+    }
+  }
+  if (spelarval == datorval) {
+    resultat.value = 'Oavgjort!'
+  } else if (
+    (spelarval == 'Sten' && datorval == 'Sax') ||
+    (spelarval == 'Sax' && datorval == 'Påse') ||
+    (spelarval == 'Påse' && datorval == 'Sten')
+  ) {
+    resultat.value = 'Du vann!'
+    score.value.spelare++
+  } else {
+    resultat.value = 'Datorn vann!'
+    score.value.dator++
+  }
+}
+function reset() {
+  score.value.spelare = 0
+  score.value.dator = 0
+  let buttons = document.getElementsByClassName('alternativ')
+  for (let b of buttons) {
+    b.classList.remove('spelarval')
+    b.classList.remove('datorval')
+  }
+}
+</script>
 
 <template>
   <header>
@@ -7,15 +68,18 @@
 
   <main>
     <div class="knapprad">
-      <button>Sten</button>
-      <button>Sax</button>
-      <button>Påse</button>
+      <button class="alternativ" @click="spelarval">Sten</button>
+      <button class="alternativ" @click="spelarval">Sax</button>
+      <button class="alternativ" @click="spelarval">Påse</button>
     </div>
     <div class="resultat">
-      <p id="resultat">Du vann!</p>
+      <p id="resultat">{{ resultat }}</p>
     </div>
     <div class="score">
-      <p><span id="spelare">0</span> - <span id="dator">0</span></p>
+      <p>
+        <span id="spelare">{{ score.spelare }}</span> - <span id="dator">{{ score.dator }}</span>
+      </p>
+      <button id="nolla" @click="reset">Nollställ Poäng</button>
     </div>
   </main>
 </template>
@@ -46,5 +110,16 @@ button {
 .score {
   font-size: 1.2em;
   text-align: center;
+}
+button.spelarval {
+  background-color: #aaff00;
+}
+button.datorval {
+  border: red solid 3px;
+}
+#nolla {
+  margin-top: 2em;
+  padding: 0.3em 0.6em;
+  font-size: 0.8em;
 }
 </style>

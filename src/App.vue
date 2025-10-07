ad41f9ff83eaa363c1d21f661386fc6715a52f8f
Ivar <63834@gymnasium.ax>
2025-10-07 10:09:00 +0300

    Flyttat logiken för poängberäkningen till en egen komponent

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue'
 import KnappRad from './components/knapprad.vue'
 import ResultatRad from './components/ResultatRad.vue'
+import PoangRad from './components/PoangRad.vue'
 
 const knappar = ref(['Sten', 'Sax', 'Påse'])
 const score = ref({ spelare: 0, dator: 0 })
@@ -9,6 +10,7 @@ const resultat = ref('Du vann!')
 const vinnare = ref('')
 
 function hittaVinnare(valdaKnappar) {
+  vinnare.value = ''
   let spelare = knappar.value.indexOf(valdaKnappar.spelare)
   let dator = knappar.value.indexOf(valdaKnappar.dator)
   resultat.value = { spelare: spelare, dator: dator }
@@ -40,12 +42,8 @@ function raknaPoang(v) {
   <main>
     <KnappRad :knappar="knappar" @valda-knappar="hittaVinnare" />
     <ResultatRad :valda-knappar="resultat" @vinnare="raknaPoang" />
-    <div class="score">
-      <p>
-        <span id="spelare">{{ score.spelare }}</span> - <span id="dator">{{ score.dator }}</span>
-      </p>
-      <button id="nolla" @click="reset">Nollställ Poäng</button>
-    </div>
+    <PoangRad :vinnare="vinnare" />
+    <button id="nolla" @click="reset">Nollställ Poäng</button>
   </main>
 </template>
 
@@ -53,11 +51,6 @@ function raknaPoang(v) {
 header {
   text-align: center;
   margin-bottom: 1.2em;
-}
-
-.score {
-  font-size: 1.2em;
-  text-align: center;
 }
 
 #nolla {
